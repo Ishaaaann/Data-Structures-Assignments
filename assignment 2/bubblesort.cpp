1db0b8918a12810f1f23ec9d8379c510ade5eefd
Ishaan Sharma <isharma_be24@thapar.edu>
2025-08-12 14:18:40 +0530

    Update bubblesort.cpp

--- assignment 2/bubblesort.cpp
+++ assignment 2/bubblesort.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,j,a[]={7,6,5,4,3,9,2,8,1},temp;
+    int i,j,a[]={64,34,25,12,22,11,90},temp;
     int n=sizeof(a)/sizeof(a[0]);
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
@@ -15,4 +15,5 @@ int main(){
     cout<<"Array sorted!!\n";
     for(i=0;i<n;i++)
     cout<<a[i]<<"\t";
+
 }
