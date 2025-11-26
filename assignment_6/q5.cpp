20c8d3e9366afb5cee9321100faa717ff9285582
Ishaan Sharma <isharma_be24@thapar.edu>
2025-11-26 18:07:49 +0530

    Update q5.cpp

--- assignment_6/q5.cpp
+++ assignment_6/q5.cpp
@@ -1,51 +1,48 @@
 #include <iostream>
 using namespace std;
-void createArray(int &r, int &c, int array[][20]){
-    cout << "enter number of rows: "<<endl;
-    cin>>r;
-    cout << "enter number of columns : "<<endl;
-    cin>>c;
-    cout << "enter elements of the array"<<endl;
-    for(int i = 0; i<r; i++){
-        for(int j=0; j<c; j++){
-            cin>>array[i][j];
-        }
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
     }
-}
-void displayArray(int &r, int &c, int array[][20]){
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            cout<<array[i][j]<<" ";
-        }
-        cout<<endl;
+};
+
+void insertEnd(Node*& head, int val) {
+    Node* newNode = new Node(val);
+    if (!head) {
+        head = newNode;
+        return;
     }
+    Node* temp = head;
+    while (temp->next)
+        temp = temp->next;
+    temp->next = newNode;
 }
-int rowsum(int &r, int &c, int array[][20], int sum){
-    int rowno; sum=0;
-    cout << "enter row number : "<<endl;
-    cin >> rowno;
-    for(int j=0; j<c; j++){
-        sum += array[rowno-1][j];
-    }
-    return sum;
+
+bool isCircular(Node* head) {
+    if (!head) return false;
+
+    Node* temp = head->next;
+    while (temp != NULL && temp != head)
+        temp = temp->next;
+
+    return (temp == head);
 }
-int columnsum(int &r, int &c, int array[][20], int sum){
-    int columnno; sum=0;
-    cout << "enter column number : "<<endl;
-    cin >> columnno;
-    for(int i=0; i<r; i++){
-        sum += array[i][columnno-1];
-    }
-    return sum;
-}
-int main(){
-    int arr[20][20], rows, columns, rsum=0, csum=0; 
-    createArray(rows, columns, arr);
-    cout << "the array is: " << endl;
-    displayArray(rows, columns, arr);
-    csum = columnsum(rows, columns, arr, csum);
-    cout << "the sum of your column number is: "<<csum<<endl;
-    rsum = rowsum(rows, columns, arr, rsum);
-    cout << "the sum of your row number is: "<<rsum<<endl;
+
+int main() {
+    Node* head = NULL;
+
+    insertEnd(head, 10);
+    insertEnd(head, 20);
+    insertEnd(head, 30);
+
+    if (isCircular(head))
+        cout << "It is a circular linked list.\n";
+    else
+        cout << "It is NOT a circular linked list.\n";
+
     return 0;
 }
